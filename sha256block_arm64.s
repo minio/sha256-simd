86b43afcb78d8d1eae14caa63db98c49c0125d45
Frank Wessels <fwessels@xs4all.nl>
2016-07-20 16:57:45 +0000

    Added LICENSE

--- sha256block_arm64.s
+++ sha256block_arm64.s
@@ -1,6 +1,22 @@
 // ARM64 version of SHA256
 
 //
+// Minio Cloud Storage, (C) 2016 Minio, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+//
 // Based on implementaion as found in  https://github.com/jocover/sha256-armv8
 //
 // Use github.com/minio/asm2plan9s on this file to assemble ARM instructions to their Plan9 equivalent
