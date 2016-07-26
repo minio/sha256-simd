b2258b77bc10931958a92dc17a29de4861703947
frankw <fwessels@xs4all.nl>
2016-07-25 22:31:42 -0700

    Fake Intel extensions for ARM64

--- cpuid.go
+++ cpuid.go
@@ -17,10 +17,6 @@
 
 package sha256
 
-func cpuid(op uint32) (eax, ebx, ecx, edx uint32)
-func cpuidex(op, op2 uint32) (eax, ebx, ecx, edx uint32)
-func xgetbv(index uint32) (eax, edx uint32)
-
 // True when SIMD instructions are available.
 var avx2 = haveAVX2()
 var avx = haveAVX()
