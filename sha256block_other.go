0d7ea3c244eec47f5b3a765685c5fcfd28031535
Dylan Dreyer <dyldreyer04@gmail.com>
2019-03-17 14:17:40 -0700

    fixing noasm tag build (#42)

--- sha256block_other.go
+++ sha256block_other.go
@@ -22,3 +22,19 @@ func blockAvxGo(dig *digest, p []byte)  {}
 func blockSsseGo(dig *digest, p []byte) {}
 func blockShaGo(dig *digest, p []byte)  {}
 func blockArmGo(dig *digest, p []byte)  {}
+
+func block(dig *digest, p []byte) {
+	if blockfunc == blockfuncSha {
+		blockShaGo(dig, p)
+	} else if blockfunc == blockfuncAvx2 {
+		blockAvx2Go(dig, p)
+	} else if blockfunc == blockfuncAvx {
+		blockAvxGo(dig, p)
+	} else if blockfunc == blockfuncSsse {
+		blockSsseGo(dig, p)
+	} else if blockfunc == blockfuncArm {
+		blockArmGo(dig, p)
+	} else if blockfunc == blockfuncGeneric {
+		blockGeneric(dig, p)
+	}
+}
