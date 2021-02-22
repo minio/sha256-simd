1b88b3ac56fa2c4a0d1b0ecee30104ec3374c7ba
Klaus Post <klauspost@gmail.com>
2021-02-22 16:19:51 +0100

    Simplyfy build tags...

--- cpuid_other.go
+++ cpuid_other.go
@@ -1,5 +1,3 @@
-// +build !arm64,!linux
-
 // Minio Cloud Storage, (C) 2021 Minio, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,8 +15,32 @@
 
 package sha256
 
-import "github.com/klauspost/cpuid/v2"
+import (
+	"bytes"
+	"io/ioutil"
+	"runtime"
+
+	"github.com/klauspost/cpuid/v2"
+)
 
 func hasArmSha2() bool {
-	return cpuid.CPU.Has(cpuid.SHA2)
+	if cpuid.CPU.Has(cpuid.SHA2) {
+		return true
+	}
+	if runtime.GOARCH != "arm64" || runtime.GOOS != "linux" {
+		return false
+	}
+
+	// Fall back to hacky cpuinfo parsing...
+	const procCPUInfo = "/proc/cpuinfo"
+
+	// Feature to check for.
+	const sha256Feature = "sha2"
+
+	cpuInfo, err := ioutil.ReadFile(procCPUInfo)
+	if err != nil {
+		return false
+	}
+	return bytes.Contains(cpuInfo, []byte(sha256Feature))
+
 }
